708248bc4898a1a49249ae23fc9c5bbacc34b283
Eduardo <panagee3@gmail.com>
2025-08-03 22:48:56 -0400

    Inclusão de Edição, Exclusão e Bloqueio ao Sistema de Cardapio

--- lib/models/Cardapio.dart
+++ lib/models/Cardapio.dart
@@ -1,32 +1,22 @@
-
 class Cardapio {
   late String _nome;
   late String _descricao;
   late double _preco;
   late String _uid;
-
+  late bool _ativo;
 
   String get nome => _nome;
-
-  set nome(String value) {
-    _nome = value;
-  }
+  set nome(String value) => _nome = value;
 
   String get descricao => _descricao;
+  set descricao(String value) => _descricao = value;
 
   double get preco => _preco;
-
-  set preco(double value) {
-    _preco = value;
-  }
-
-  set descricao(String value) {
-    _descricao = value;
-  }
+  set preco(double value) => _preco = value;
 
   String get uid => _uid;
+  set uid(String value) => _uid = value;
 
-  set uid(String value) {
-    _uid = value;
-  }
+  bool get ativo => _ativo;
+  set ativo(bool value) => _ativo = value;
 }
